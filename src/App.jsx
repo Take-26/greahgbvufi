84a75dabca3fdfdd328a4dd16afb567270a025c7
Taabaldy <koshokbaevt1@gmail.com>
2024-05-26 15:26:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import {API} from "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"
+
+const App = () => {
+  return (
+    <div>
+        <Main />
+    </div>
+  )
+}
+
+export default App
